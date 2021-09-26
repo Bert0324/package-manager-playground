420adc19b8c91bf8b9df673011ca8077f48700e2
yuchen.huang <hyc231921@alibaba-inc.com>
2021-09-26 20:08:10 +0800

    feat: init

---
+++ pnpm/index.js
@@ -0,0 +1,4 @@
+const express = require('express');
+
+console.log(require.resolve('.pnpm/body-parser@1.19.0/node_modules/body-parser/index.js'));
+console.log(express.name);
---
+++ yarn/index.js
@@ -0,0 +1,3 @@
+const bodyParser = require('body-parser');
+
+console.log(bodyParser);
